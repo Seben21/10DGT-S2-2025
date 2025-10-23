e8d65917fb9738379b1235e7a37bfefa47ae4511
Seben <zac.fryer@kingsway.school.nz>
2025-10-24 10:22:30 +1300

    Main - Coffee program
    Added functions and multiple choices

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ if like_coffee == "Yes":
 else:
     print("You are misssing out! Why not give it a try?")'''
 
-# While loop - Version 2.0
+''' # While loop - Version 2.0
 keep_going = ""
 while keep_going == "":
     like_coffee = input("Do you like coffee? ")
@@ -22,4 +22,25 @@ while keep_going == "":
     elif like_coffee == "No":
         print("You are misssing out! Why not give it a try?")
     else:
-        print("Error, please type either Yes or No")
+        print("Error, please type either Yes or No") '''
+
+def coffee_program():
+    keep_going = ""
+    while keep_going == "":
+        like_coffee = input("Do you like coffee? ").lower()
+        if like_coffee == "yes" or like_coffee == "y":
+            print("Thats great, I like coffee too!")
+        elif like_coffee == "no" or like_coffee == "n":
+            print("Thats too bad")
+            like_tea = input("Do you like tea instead? ").lower()
+            if like_tea == "yes" or like_tea == "y":
+                print("Good for you")
+            elif like_tea == "no" or like_tea == "n":
+                print("You picky little ****, do you even like anything???")
+        else:
+            print("I don't understand. Please answer with either a yes or a no.")
+    
+        keep_going = input("Press any key to continue. Press <Enter> to repeat.")
+
+if __name__ == "__main__":
+    coffee_program()
