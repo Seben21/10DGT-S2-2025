6e73d3291f470866fa5e7ce1f3878b1d12316c34
Seben <zac.fryer@kingsway.school.nz>
2025-10-29 16:44:49 +1300

    Area and Perimeter Calculator
    Made it so that it can also calculate 3D shapes - Volume and Surface Area

--- Area and Perimeter calculator.py
+++ Area and Perimeter calculator.py
@@ -1,15 +1,74 @@
 # Author: Zach Fryer
 # Title: Area/Perimeter Calculator
 # Date: 29/10/2025
-# Version 1.0
+# Version 1.2
 
 print("Hello User, welcome to my Area/Perimeter calculator! \nPlease answer the following questions so I can calculate for you!\n\n")
 dimensions = input("Is your shape 2D or 3D? ").lower()
+while True:
+    if dimensions == "2d":
+        while True:
+            try:
+                length = float(input(f"Please enter the length of the shape: "))
+                if length <= 0:
+                    print("Please enter a number above 0\n")
+                else:
+                    break
+            except ValueError:
+                print(f"Please enter a value\n")
+        print()
+        while True:
+            try:
+                width = float(input(f"Please enter the width of the shape: "))
+                if width <=0:
+                    print("Please enter a number above 0\n")
+                else:
+                    break
+            except ValueError:
+                print(f"Please enter a value\n")
+        print()
 
-if dimensions == "2d":
-    length = float(input(f"Please enter the length of the shape: "))
-    width = float(input(f"Please enter the width of the shape: "))
-    area = length * width
-    perimeter = length * 2 + width * 2
-    print(f"Area = {area}\nPerimeter = {perimeter}")
-    
+        while True:
+            area = length * width
+            perimeter = length * 2 + width * 2
+            print(f"Area = {area}\nPerimeter = {perimeter}\n\n\n")
+            break
+
+        input("Would you like to continue?\nPress any key to continue\nPress <ENTER> to repeat")
+
+    elif dimensions == "3d":
+        while True:
+            try:
+                length = float(input(f"Please enter the length of the shape: "))
+                if length <= 0:
+                    print("Please enter a number above 0\n")
+                else:
+                    break
+            except ValueError:
+                print(f"Please enter a value\n")
+        print()
+        while True:
+            try:
+                width = float(input(f"Please enter the width of the shape: "))
+                if width <=0:
+                    print("Please enter a number above 0\n")
+                else:
+                    break
+            except ValueError:
+                print(f"Please enter a value\n")
+        print()
+        while True:
+                    try:
+                        height = float(input(f"Please enter the width of the shape: "))
+                        if height <=0:
+                            print("Please enter a number above 0\n")
+                        else:
+                            break
+                    except ValueError:
+                        print(f"Please enter a value\n")
+        
+        while True:
+            surface_area = 2 * (height*width + width*length + length*height)
+            volume = length * height * width
+            print(f"Surface Area = {surface_area}\nVolume = {volume}\n\n\n")
+            break
