d2e2793e5c2c31251596f2fed452b263a4587744
Seben <zac.fryer@kingsway.school.nz>
2025-10-31 09:54:00 +1300

    Area/Perimeter calculator
    Made the output more grammatically accurate

--- Area and Perimeter calculator.py
+++ Area and Perimeter calculator.py
@@ -1,13 +1,13 @@
 # Author: Zach Fryer
 # Title: Area/Perimeter Calculator
-# Date: 29/10/2025
-# Version 1.5
+# Date: 31/10/2025
+# Version 1.6
 
-print("Hello User, welcome to my Area/Perimeter calculator! \nPlease answer the following questions so I can calculate for you!\n\n")
+print("Hello User, welcome to my Area/Perimeter calculator! \nPlease answer the following questions so I can calculate for you!")
 repeat = ""
 while repeat == "":
     try:
-        dimensions = input("Is your shape 2D or 3D? ").lower()
+        dimensions = input("\n\nIs your shape 2D or 3D? ").lower()
         if dimensions == "2d":
             while True:
                 try:
@@ -32,9 +32,9 @@ while repeat == "":
             while True:
                 area = length * width
                 perimeter = length * 2 + width * 2
-                print(f"Area = {area} cm\nPerimeter = {perimeter} cm\n\n")
+                print(f"Area = {area} cm²\nPerimeter = {perimeter} cm\n\n")
                 break
-            repeat = input("Would you like to continue?\nPress any key to continue,\nPress <ENTER> to repeat:  ")
+            repeat = input("Would you like to continue?\nPress <ENTER> to repeat:  ")
         elif dimensions == "3d":
             while True:
                 try:
@@ -69,11 +69,11 @@ while repeat == "":
             while True:
                 surface_area = 2 * (height*width + width*length + length*height)
                 volume = length * height * width
-                print(f"Surface Area = {surface_area} cm\nVolume = {volume} cm\n\n")
+                print(f"Surface Area = {surface_area} cm²\nVolume = {volume} cm³\n\n")
                 break
-            repeat = input("Would you like to continue?\nPress any key to continue,\nPress <ENTER> to repeat:  ")
+            repeat = input("Would you like to continue?\nPress <ENTER> to repeat:  ")
         else:
-            repeat = input("ERROR - Please enter either <2D> or <3D>!!!! \nPress <ENTER> to repeat: \n\n")
+            print("ERROR - Please enter either <2D> or <3D>!!!! \n")
     except ValueError:
         repeat = ''
         print("\n")
