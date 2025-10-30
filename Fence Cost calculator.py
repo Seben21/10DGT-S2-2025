5643f30c4a74f8e65fb9430a983d971711f12e43
Seben <zac.fryer@kingsway.school.nz>
2025-10-31 10:47:15 +1300

    Fence Cost calculator
    Recycled the number checker, made it loop properly

--- Fence Cost calculator.py
+++ Fence Cost calculator.py
@@ -1,19 +1,40 @@
 # Author: Zach Fryer
 # Title: Fence cost calculator
 # Date: 31/10/2025
-# Version 1.0
+# Version 1.5
 
-print("Hello User, welcome to my fence cost calculator!\nPlease answer all the following questions so I can calculate for you:\n")
+print("Hello User, welcome to my fence cost calculator!\nPlease answer all the following questions so I can calculate for you:")
 repeat = ''
 while repeat == '':
-    try:
-        fence_cost = float(input("What is the cost per metre of said fence? "))
-        width = float(input("\nWhat is the width of the desired area to be fenced, in metres? "))
-        length = float(input("\nWhat is the length of the desired area to be fenced, in metres? "))
-
-        perimeter = (width + length) * 2
-        total_cost = perimeter * fence_cost
-        print(f"\n\nThe total price for the fence is ${total_cost} ")
-        repeat = input("\n\nPress <ENTER> if you wish to repeat: ")
-    except ValueError:
-        print("\nThats not a valid value")
+    while True:
+        try:
+            fence_cost = float(input("\n\nWhat is the cost per metre of said fence? "))
+            if fence_cost <= 0:
+                print("Please enter a value above 0")
+            else: 
+                break
+        except ValueError:
+            print("Please enter a number")
+    while True:
+        try:
+            width = float(input("\nWhat is the width of the desired area to be fenced, in metres? "))
+            if width <= 0:
+                print("Please enter a value above 0")
+            else: 
+                break
+        except ValueError:
+            print("Please enter a number")
+    while True:
+        try:
+            length = float(input("\nWhat is the length of the desired area to be fenced, in metres? "))
+            if length <= 0:
+                print("Please enter a value above 0")
+            else: 
+                break
+        except ValueError:
+            print("Please enter a number")
+    print()
+    perimeter = (width + length) * 2
+    total_cost = perimeter * fence_cost
+    print(f"\n\nThe total price for the fence is ${total_cost} ")
+    repeat = input("\n\nPress <ENTER> if you wish to repeat: ")
