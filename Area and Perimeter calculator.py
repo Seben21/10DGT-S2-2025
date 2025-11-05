a1a67e1a9523025e3f717ed4b81b2fd971a990d6
Seben <zac.fryer@kingsway.school.nz>
2025-11-05 19:38:23 +1300

    Area and perimeter calc / Fence cost calc
    Added good comments to describe what the parts of code do

--- Area and Perimeter calculator.py
+++ Area and Perimeter calculator.py
@@ -1,17 +1,17 @@
 # Author: Zach Fryer
 # Title: Area/Perimeter Calculator
 # Date: 31/10/2025
-# Version 1.6
+# Version 1.7
 
-print("Hello User, welcome to my Area/Perimeter calculator! \nPlease answer the following questions so I can calculate for you!")
+print("Hello User, welcome to my Area/Perimeter calculator! \nPlease answer the following questions so I can calculate for you!") # Introduction
 repeat = ""
 while repeat == "":
     try:
-        dimensions = input("\n\nIs your shape 2D or 3D? ").lower()
+        dimensions = input("\n\nIs your shape 2D or 3D? ").lower() # Get dimensions required
         if dimensions == "2d":
             while True:
                 try:
-                    length = float(input(f"Please enter the length of the shape in cm: "))
+                    length = float(input(f"Please enter the length of the shape in cm: ")) # Get length 2d
                     if length <= 0:
                         print("Please enter a number above 0\n")
                     else:
@@ -21,7 +21,7 @@ while repeat == "":
             print()
             while True:
                 try:
-                    width = float(input(f"Please enter the width of the shape in cm: "))
+                    width = float(input(f"Please enter the width of the shape in cm: ")) # Get width 2d
                     if width <=0:
                         print("Please enter a number above 0\n")
                     else:
@@ -30,15 +30,15 @@ while repeat == "":
                     print(f"Please enter a value\n")
             print()
             while True:
-                area = length * width
-                perimeter = 2 * (length + width)
+                area = length * width # Calculate area
+                perimeter = 2 * (length + width) # Calculate perimeter
                 print(f"Area = {area} cm²\nPerimeter = {perimeter} cm\n\n")
                 break
-            repeat = input("Would you like to continue?\nPress <ENTER> to repeat:  ")
+            repeat = input("Would you like to continue?\nPress <ENTER> to repeat:  ") # Loop
         elif dimensions == "3d":
             while True:
                 try:
-                    length = float(input(f"Please enter the length of the shape in cm: "))
+                    length = float(input(f"Please enter the length of the shape in cm: ")) # Get length 3d
                     if length <= 0:
                         print("Please enter a number above 0\n")
                     else:
@@ -48,7 +48,7 @@ while repeat == "":
             print()
             while True:
                 try:
-                    width = float(input(f"Please enter the width of the shape in cm: "))
+                    width = float(input(f"Please enter the width of the shape in cm: ")) # Get width 3d
                     if width <=0:
                         print("Please enter a number above 0\n")
                     else:
@@ -58,7 +58,7 @@ while repeat == "":
             print()
             while True:
                         try:
-                            height = float(input(f"Please enter the height of the shape in cm: "))
+                            height = float(input(f"Please enter the height of the shape in cm: ")) # Get height 3d
                             if height <=0:
                                 print("Please enter a number above 0\n")
                             else:
@@ -67,13 +67,13 @@ while repeat == "":
                             print(f"Please enter a value\n")
             print()
             while True:
-                surface_area = 2 * (height*width + width*length + length*height)
-                volume = length * height * width
+                surface_area = 2 * (height*width + width*length + length*height) # Calculate surface area
+                volume = length * height * width # Calculate volume
                 print(f"Surface Area = {surface_area} cm²\nVolume = {volume} cm³\n\n")
                 break
-            repeat = input("Would you like to continue?\nPress <ENTER> to repeat:  ")
+            repeat = input("Would you like to continue?\nPress <ENTER> to repeat:  ") # Loop
         else:
-            print("ERROR - Please enter either <2D> or <3D>!!!! \n")
+            print("ERROR - Please enter either <2D> or <3D>!!!! \n") 
     except ValueError:
         repeat = ''
         print("\n")
--- Fence Cost calculator.py
+++ Fence Cost calculator.py
@@ -1,14 +1,14 @@
 # Author: Zach Fryer
 # Title: Fence cost calculator
 # Date: 31/10/2025
-# Version 1.5
+# Version 1.6
 
-print("Hello User, welcome to my fence cost calculator!\nPlease answer all the following questions so I can calculate for you:")
+print("Hello User, welcome to my fence cost calculator!\nPlease answer all the following questions so I can calculate for you:") # intro
 repeat = ''
 while repeat == '':
     while True:
         try:
-            fence_cost = float(input("\n\nWhat is the cost per metre of said fence? "))
+            fence_cost = float(input("\n\nWhat is the cost per metre of said fence? ")) # Get cost
             if fence_cost <= 0:
                 print("Please enter a value above 0")
             else: 
@@ -17,7 +17,7 @@ while repeat == '':
             print("Please enter a number")
     while True:
         try:
-            width = float(input("\nWhat is the width of the desired area to be fenced, in metres? "))
+            width = float(input("\nWhat is the width of the desired area to be fenced, in metres? ")) # Get width
             if width <= 0:
                 print("Please enter a value above 0")
             else: 
@@ -26,7 +26,7 @@ while repeat == '':
             print("Please enter a number")
     while True:
         try:
-            length = float(input("\nWhat is the length of the desired area to be fenced, in metres? "))
+            length = float(input("\nWhat is the length of the desired area to be fenced, in metres? ")) # Get length
             if length <= 0:
                 print("Please enter a value above 0")
             else: 
@@ -34,7 +34,7 @@ while repeat == '':
         except ValueError:
             print("Please enter a number")
     print()
-    perimeter = (width + length) * 2
-    total_cost = perimeter * fence_cost
-    print(f"\n\nThe total price for the fence is ${total_cost} ")
-    repeat = input("\n\nPress <ENTER> if you wish to repeat: ")
+    perimeter = (width + length) * 2 # Calculate perimeter
+    total_cost = perimeter * fence_cost # Calculate total
+    print(f"\n\nThe total price to fully fence the entire area is ${total_cost} ")
+    repeat = input("\n\nPress <ENTER> if you wish to repeat: ") # Loop program
